30f35d9e94086078a78ec77e6a6efd34d18470d5
Samy Pessé <samypesse@gmail.com>
2015-01-30 13:13:10 +0100

    Adapt plugin for gitbook 2.x.x

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var katex = require("katex");
-var cheerio = require("cheerio");
 
 module.exports = {
     book: {
@@ -15,27 +14,16 @@ module.exports = {
             "katex.min.css"
         ]
     },
-    hooks: {
-        page: function(page) {
-            for (var i in page.sections) {
-                section = page.sections[i];
-                if ( section.type != "normal" ) continue;
-
-                var $ = cheerio.load(section.content);
-                $("script").each(function() {
-                    // Check is math
-                    var type = $(this).attr("type") || "";
-                    if (type.indexOf("math/tex") < 0) return;
-
-                    var math = $(this).html();
-                    $(this).replaceWith(katex.renderToString(math));
-                });
-
-                // Replace by transform
-                section.content = $.html();
+    blocks: {
+        math: {
+            shortcuts: {
+                parsers: ["markdown"],
+                start: "$$",
+                end: "$$"
+            },
+            process: function(blk) {
+                return katex.renderToString(blk.body);
             }
-
-            return page;
         }
     }
 };
